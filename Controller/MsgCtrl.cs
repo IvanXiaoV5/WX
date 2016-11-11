dbad73d19965bc2958df93af6e05e1673d13ad5d
xiaoxiong3533521@163.com <xiaoxiong3533521@163.com>
2016-11-11 11:02:32 +0800

    555

---
+++ Controller/MsgCtrl.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using Models;
+using Helper;
+
+namespace Controller
+{
+    public class MsgCtrl
+    {
+        private WXServices ser = new WXServices();
+
+        /// <summary>
+        /// 把接收到的文本消息又发送回去
+        /// </summary>
+        /// <param name="context"></param>
+        public void RecAndSend(HttpContext context)
+        {
+            TextMsg msg = ser.Received_TextToModel(context);
+            string xmlstr = ser.SendText(msg);
+            context.Response.Write(xmlstr);
+        }
+
+        public string PushMenu(string appid,string appsecret,string MenuJson)
+        {
+            
+            string weixin1 = "";
+            weixin1 += "{\n";
+            weixin1 += "\"button\":[\n";
+            weixin1 += "{\n";
+            weixin1 += "\"type\":\"click\",\n";
+            weixin1 += "\"name\":\"今日歌曲\",\n";
+            weixin1 += "\"key\":\"V1001_TODAY_MUSIC123eee\"\n";
+            weixin1 += "},\n";
+            weixin1 += "{\n";
+            weixin1 += "\"type\":\"click\",\n";
+            weixin1 += "\"name\":\"歌手简介\",\n";
+            weixin1 += "\"key\":\"V1001_TODAY_SINGER123eee\"\n";
+            weixin1 += "},\n";
+            weixin1 += "{\n";
+            weixin1 += "\"name\":\"菜单\",\n";
+            weixin1 += "\"sub_button\":[\n";
+            weixin1 += "{\n";
+            weixin1 += "\"type\":\"click\",\n";
+            weixin1 += "\"name\":\"hello word\",\n";
+            weixin1 += "\"key\":\"V1001_HELLO_WORLD123eee\"\n";
+            weixin1 += "},\n";
+            weixin1 += "{\n";
+            weixin1 += "\"type\":\"click\",\n";
+            weixin1 += "\"name\":\"赞一下我们\",\n";
+            weixin1 += "\"key\":\"V1001_GOOD123eee\"\n";
+            weixin1 += "}]\n";
+            weixin1 += "}]\n";
+            weixin1 += "}\n";
+
+            MenuJson = MenuJson == "" ? weixin1 : MenuJson;
+            string access = ser.GetAccess_token(appid, appsecret);
+            string context= ser.PushMenu(access, MenuJson);
+            return context;
+        }
+    }
+}
---
+++ 微信功能了解/bll/MsgCall.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Dos.WeChat;
+
+namespace web.bll
+{
+    public class MsgCall : IMsgCall
+    {
+        public void AfterMsgCall(ReceiveMsg msg, ResponseMsg repMsg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ResponseMsg EventMsgCall(RecEventMsg msg)
+        {
+            var r = msg.GetTextResponse();
+            switch (msg.Event.ToLower())
+            {
+                //关注公众号事件
+                case RecEventMsg.Subscribe:
+                    r.Data = (TextMsgData)("欢迎关注本公众号！");
+                    break;
+                //菜单点击事件
+                case RecEventMsg.Click:
+                    //if (msg.EventKey == "TestClick")
+                    //{
+                    //    r.Data = (TextMsgData)("您点击了测试菜单按钮！");
+                    //}
+                    r.Data = (TextMsgData)("您点击了测试菜单按钮！");
+                    break;
+                //二维码扫描事件
+                case RecEventMsg.Scan:
+                    r.Data = (TextMsgData)("您扫描了二维码！场景值：" + msg.EventKey);
+                    break;
+
+                default:
+                    r.Data = (TextMsgData)("default");
+                    break;
+                
+            }
+            return r;
+        }
+
+        public ResponseMsg ImageMsgCall(RecImgMsg msg)
+        {
+            var r = msg.GetTextResponse();
+            r.Data = (TextMsgData)("收到您的图片了");
+            return r;
+        }
+
+        public ResponseMsg LinkMsgCall(RecLinkMsg msg)
+        {
+            var r = msg.GetTextResponse();
+            r.Data = (TextMsgData)("收到您的链接了");
+            return r;
+        }
+
+        public ResponseMsg LocationMsgCall(RecLocationMsg msg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ResponseMsg TextMsgCall(RecTextMsg msg)
+        {
+            //回复文本消息
+            var r = msg.GetTextResponse();
+            r.Data = (TextMsgData)(string.Format("已收到您的消息！您的OpenId：{0}。您发送的内容为：{1}。",
+                        msg.FromUserName, msg.Content));
+            return r;
+            //throw new NotImplementedException();
+        }
+
+        public ResponseMsg VoiceMsgCall(RecVoiceMsg msg)
+        {
+            var r = msg.GetTextResponse();
+            r.Data = (TextMsgData)("收到您的语音了");
+            return r;
+        }
+    }
+}
